a94eed6420a260bd0e70cff2a871720c38a63cdc
Tom H <snarfblam@gmail.com>
2018-04-26 22:33:57 -0400

    Moved header to its own component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
 
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <nav className="navbar navbar-dark bg-dark justify-content-between">
-                    <h1 className="navbar-brand">
-                        Clickeroonie-doonie
-                    </h1>
-                    <p>
-                        Do the clickening
-                    </p>
-                    <p>
-                        Score: Doesn't exist yet
-                    </p>
-                </nav>
+                <Header />
                 <p className="App-intro">
                     To get started, edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+var Header = props => (
+    <nav className="navbar navbar-dark bg-dark justify-content-between">
+        <h1 className="navbar-brand">
+            Clickeroonie-doonie
+        </h1>
+        <p>
+            Do the clickening
+        </p>
+        <p>
+            Score: Doesn't exist yet
+        </p>
+    </nav>
+);
+
+export default Header;
