5046bd083a9ea7f1853616bad3edea8d02570ea9
Tom H <snarfblam@gmail.com>
2018-04-26 22:31:48 -0400

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,19 +3,26 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+    render() {
+        return (
+            <div className="App">
+                <nav className="navbar navbar-dark bg-dark justify-content-between">
+                    <h1 className="navbar-brand">
+                        Clickeroonie-doonie
+                    </h1>
+                    <p>
+                        Do the clickening
+                    </p>
+                    <p>
+                        Score: Doesn't exist yet
+                    </p>
+                </nav>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-      </div>
-    );
-  }
+            </div>
+        );
+    }
 }
 
 export default App;
