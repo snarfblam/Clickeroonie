f384667366c68bd139bad24472a2086f8989518f
Tom H <snarfblam@gmail.com>
2018-04-28 18:10:58 -0400

    Added image files

--- src/App.js
+++ src/App.js
@@ -2,17 +2,54 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
+import ImageGrid from './components/ImageGrid' 
 
 class App extends Component {
+    constructor() {
+        super();
+        
+        this.state = {
+            images: [
+                img('desgeega.gif', 0),
+                img('holtz.gif', 1),
+                img('geruta.gif', 2),
+                img('mella.gif', 3),
+                img('mellow.gif', 4),
+                img('memu.gif', 5),
+                img('rio.gif', 6),
+                img('zebbo.gif', 7),
+                img('zoomer.gif', 8),
+                img('nova.gif', 9),
+                img('zeb.gif', 10),
+                img('squeept.gif', 11),
+            ]
+        }
+    }
+
     render() {
         return (
             <div className="App">
                 <Header />
-                <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+                <h1>Clickeroonie-Doonie</h1>
+                <ImageGrid />
             </div>
         );
+    }
+}
+
+function img(name, id, clicked) {
+    return {
+        url: '/images/' + name,
+        clicked: clicked || false, // undefined --> false
+        id: id,
+        setClicked: function (value) {
+            return {
+                url: this.url,
+                clicked: value || false,
+                id: this.is,
+                setClicked: this.setClicked
+            }
+        },
     }
 }
 
---
+++ src/components/ImageGrid.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+var ImageGrid = props => (
+    <div className='container'>
+        <div className='row'>
+            <div className="col-3">
+                a
+            </div>
+            <div className="col-3">
+                b
+            </div>
+            <div className="col-3">
+                c
+            </div>
+            <div className="col-3">
+                d
+            </div>
+        </div>
+    </div>
+);
+
+export default ImageGrid;
