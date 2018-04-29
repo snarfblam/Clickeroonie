8935dc5aee9d8c5cdefd321106d130e336c6c96c
Tom H <snarfblam@gmail.com>
2018-04-29 15:54:59 -0400

    Extracted ImageCard component from ImageGrid

---
+++ src/components/ImageCard.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+var ImageCard = props => {
+    var imgInfo = props.imgInfo
+
+    var imgClass = 'align-middle enemy-image';
+    var enemyCardClass = 'enemy-card p-1';
+
+    if (props.shrunken) imgClass += ' enemy-shrink';
+    if (props.dim) imgClass += ' enemy-dim';
+    if (props.dim) enemyCardClass += ' enemy-card-dim';
+    if (props.fail) enemyCardClass += ' enemy-card-fail';
+
+    return (
+        <div className="col-3 p-2" key={imgInfo.id}>
+            <div className={enemyCardClass}>
+                <a href="#enemy" data-id={imgInfo.id} onClick={props.clickHandler}>
+                    <div className="enemy-container p-1">
+                        <img src={imgInfo.url} className={imgClass} alt={imgInfo.name} />
+                    </div>
+                </a>
+            </div>
+        </div>
+    );    
+};
+
+export default ImageCard;
--- src/components/ImageGrid.js
+++ src/components/ImageGrid.js
@@ -1,31 +1,20 @@
 import React from 'react';
+import ImageCard from './ImageCard';
 
 var ImageGrid = props => {
 
     return (
         <div className='container'>
             <div className='row'>
-                {props.images.map(imgInfo => {
-                    var imgClass = 'align-middle enemy-image';
-                    if (props.shrunken) imgClass += ' enemy-shrink';
-                    if (props.gameOver && imgInfo.clicked && imgInfo.id != props.loosingId) imgClass += ' enemy-dim';
-
-                    var enemyCardClass = 'enemy-card p-1';
-                    if (imgInfo.id == props.loosingId) enemyCardClass += ' enemy-card-fail'
-                    return (
-                        <div className="col-3 p-2" key={imgInfo.id}>
-                            <div className={enemyCardClass}>
-                                <a href="#enemy" data-id={imgInfo.id} onClick={props.clickHandler}>
-                                    <div className="enemy-container p-1">
-                                        {
-                                            <img src={imgInfo.url} className={imgClass} alt={imgInfo.name} />
-                                        }
-                                    </div>
-                                </a>
-                            </div>
-                        </div>
-                    )
-                })}    
+                {props.images.map(imgInfo => (
+                    <ImageCard
+                        dim={props.gameOver && imgInfo.clicked && imgInfo.id != props.loosingId}
+                        fail={imgInfo.id == props.loosingId}
+                        imgInfo={imgInfo}
+                        clickHandler={props.clickHandler}
+                        shrunken={props.shrunken}
+                    />
+                ))}    
             </div>
         </div>
     )};
